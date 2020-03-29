3f2f2eb0bed0cf0729426bd86e269e8d9bed4498
Oliver Ponder <oliver.ponder@gmail.com>
2020-03-30 01:32:14 +0800

    Work on the games list.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import styled from '@emotion/styled'
+import { keyframes } from '@emotion/core'
+
+const borderGlow = keyframes`
+  0% { border-color: #43B0A1 }
+  50% { border-color: #92FFDC }
+  100% { border-color: #43B0A1 }
+`
 
 const Wrapper = styled('div')`
   width: 250px;
-  height: 300px;
   margin-right: 20px;
   display: inline-block;
 `;
@@ -12,18 +18,48 @@ const GameIcon = styled('div')`
   width: 250px;
   height: 250px;
   background-color: #444;
+
+  &.selected {
+    border: 8px solid #53C0B1;
+    border-radius: 4px;
+    position: relative;
+    top: -8px;
+
+    animation: ${borderGlow} 1s 0s infinite;
+  }
+
+  &.selected::before {
+    position: absolute;
+    top: -4px;
+    left: -4px;
+    border: 4px solid #222;
+    display: block;
+    width: 250px;
+    height: 250px;
+    content: '';
+  }
 `;
 
 const Title = styled('div')`
   overflow: hidden;
   width: 200px;
+  color: #66CDC4;
+  font-size: 20px;
+  margin-bottom: 10px;
+  font-weight: bold;
 `;
 
-export interface Props { selected: boolean}
+export interface Props { selected: boolean, title: string, onClick: () => void, forwardedRef: any}
 
 export default (props: Props) => {
-  return <Wrapper>
-    <Title>Animal Crossing</Title>
-    <GameIcon />
+  return <Wrapper onClick={props.onClick} ref={props.forwardedRef}>
+    {
+      props.selected && <Title>{props.title}</Title>
+    }
+
+    {
+      !props.selected && <Title>&nbsp;</Title>
+    }
+    <GameIcon className={props.selected ? 'selected' : undefined} />
   </Wrapper>;
 }
--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from '@emotion/styled'
 import Game from './Game';
 
 const Wrapper = styled('div')`
   margin-top: 40px;
-  height: 275px;
   position: absolute;
   left: 0px;
   overflow-y: hidden;
@@ -12,28 +11,56 @@ const Wrapper = styled('div')`
 `;
 
 const List = styled('div')`
-  padding-left: 50px;
-  padding-right: 50px;
+  padding-left: 45px;
+  padding-right: 45px;
   white-space: nowrap;
   overflow-y: hidden;
   width: fit-content;
-  height: 275px;
 `;
 
 export interface Props {}
 
+interface IGame {title: string, selected: boolean, ref: any}
+
 export default (props: Props) => {
-  return <Wrapper>
+  const listRef = useRef(null);
+
+  const [games, setGames]: [IGame[], any] = useState([
+    {title: "Animal Crossing", selected: true, ref: useRef(null)},
+    {title: "Zelda", selected: false, ref: useRef(null)},
+    {title: "Mario Kart", selected: false, ref: useRef(null)},
+    {title: "Pokemon", selected: false, ref: useRef(null)},
+    {title: "Tetris", selected: false, ref: useRef(null)},
+    {title: "Overcooked", selected: false, ref: useRef(null)},
+    {title: "Devil May Cry", selected: false, ref: useRef(null)},
+  ]);
+
+  const select = (title: string) => {
+    let selectedGame: IGame = null;
+
+    let ga = games.map(g => {
+      if (g.title === title) {
+        selectedGame = g;
+        return Object.assign({}, g, {selected: true})
+      }
+      return Object.assign({}, g, {selected: false})}
+    );
+
+    if(selectedGame.ref !== null) {
+      console.log(selectedGame.ref.current.offsetLeft);
+      listRef.current.scrollLeft = selectedGame.ref.current.offsetLeft - 40;
+    }
+
+    setGames(ga);
+  }
+
+  console.log('rendered games list');
+
+  return <Wrapper ref={listRef}>
     <List>
-      <Game selected={true}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
-      <Game selected={false}/>
+      {
+        games.map(g => <Game forwardedRef={g.ref} key={g.title} selected={g.selected} title={g.title} onClick={select.bind(undefined, g.title)} />)
+      }
     </List>
   </Wrapper>;
 }
